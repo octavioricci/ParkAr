4653775005e10f5b50539e7ba1c8947f3b26cd39
octavioricci <octavioricci@gmail.com>
2017-05-26 23:34:53 -0300

    Add files via upload

---
+++ ViewModels/ReservaBoxViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ParkAr.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkAr.ViewModels
+{
+    public class ReservaBoxViewModel
+    {
+        [Display(Name = "Nombre Cliente")]
+        public Cliente Cliente { get; set; }
+        [Display(Name = "Numero Box")]
+        public Box BoxSeleccionado { get; set; }
+        [Display(Name = "Estacionamiento")]
+
+        public Estacionamiento EstacionamientoSeleccionado { get; set; }
+        [Display(Name = "Horas")]
+        public List<String> Horas { get; set; }
+        public byte CantidadHoras { set; get; }
+        [Display(Name = "Estacionamientos")]
+        public List<Estacionamiento> Estacionamientos { set; get; }
+
+    }
+}
